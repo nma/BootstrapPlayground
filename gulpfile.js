7fc9d9d46e517c37b74a9e16d6ade55ecce36877
Nick Ma <nickma38@gmail.com>
2017-04-10 12:30:16 -0400

    modify to use gulp-livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,35 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var cssbeautify = require('gulp-cssbeautify');
+var nodemon = require('gulp-nodemon');
+var notify = require('gulp-notify');
+var livereload = require('gulp-livereload');
 
 gulp.task('express', function() {
   var express = require('express');
   var app = express();
   app.use(require('connect-livereload')({port: 35729}));
   app.use(express.static(__dirname));
-  app.listen(3000, '0.0.0.0');
+  app.listen(3030, '0.0.0.0');
 });
 
-var tinylr;
 gulp.task('livereload', function() {
-  tinylr = require('tiny-lr')();
-  tinylr.listen(35729);
+  livereload.listen(35729);
+});
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cssbeautify())
+    .pipe(gulp.dest('./css'));
 });
 
 function notifyLiveReload(event) {
   var fileName = require('path').relative(__dirname, event.path);
 
-  tinylr.changed({
-    body: {
-      files: [fileName]
-    }
-  });
+  gulp.src(fileName).pipe(livereload());
 }
 
-gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
-});
- 
 gulp.task('watch', function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
   gulp.watch('*.html', notifyLiveReload);
